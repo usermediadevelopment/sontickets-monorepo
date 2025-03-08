1da167c730ef5c1d78e7a7dfd43eb7d991849ea0
Carlos <arangotorcar@gmail.com>
2025-03-08 18:26:11 -0500

    feat: add development dataset

--- apps/mejores-restaurantes-sanity/sanity.cli.ts
+++ apps/mejores-restaurantes-sanity/sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '7d9e3dzz',
-    dataset: 'production',
+    dataset: process.env.SANITY_STUDIO_API_DATASET || 'production',
   },
   /**
    * Enable auto-updates for studios.
