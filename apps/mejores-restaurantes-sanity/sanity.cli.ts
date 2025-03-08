f4a2ab22377f210b8ce046832b45b69005dbbb7a
Carlos <carlos.arango@usermedia.co>
2025-03-08 18:57:44 -0500

    chore: remove extra code

--- apps/mejores-restaurantes-sanity/sanity.cli.ts
+++ apps/mejores-restaurantes-sanity/sanity.cli.ts
@@ -5,9 +5,5 @@ export default defineCliConfig({
     projectId: '7d9e3dzz',
     dataset: process.env.SANITY_STUDIO_API_DATASET || 'production',
   },
-  /**
-   * Enable auto-updates for studios.
-
-   */
   autoUpdates: true,
 })
