02d3102128dca8331c3541cc625b3087dd8c4eb3
Carlos <arangotorcar@gmail.com>
2025-03-08 18:50:31 -0500

    new commit

--- apps/mejores-restaurantes-sanity/sanity.cli.ts
+++ apps/mejores-restaurantes-sanity/sanity.cli.ts
@@ -7,7 +7,7 @@ export default defineCliConfig({
   },
   /**
    * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+
    */
   autoUpdates: true,
 })
