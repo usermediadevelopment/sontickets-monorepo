ee86260e0f086cad381ece4123b4c18658805b6b
Carlos <carlos.arango@usermedia.co>
2025-03-11 06:11:15 -0500

    feat: set up sanity environemnts in ci

--- apps/mejores-restaurantes-sanity/sanity.cli.ts
+++ apps/mejores-restaurantes-sanity/sanity.cli.ts
@@ -3,7 +3,8 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '7d9e3dzz',
-    dataset: process.env.SANITY_STUDIO_API_DATASET || 'production',
+    dataset: process.env.SANITY_STUDIO_API_DATASET,
   },
   autoUpdates: true,
+  studioHost: process.env.SANITY_STUDIO_HOST,
 })
