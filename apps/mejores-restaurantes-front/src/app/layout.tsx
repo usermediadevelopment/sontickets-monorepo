f798a39639228f742865f83fd455faa13b1442ec
Carlos Arango <carlos.arango@usermedia.co>
2025-03-25 15:34:04 -0500

    feat: init nextjs+ tailwind+shadcnui

---
+++ apps/mejores-restaurantes-front/src/app/layout.tsx
@@ -0,0 +1,22 @@
+
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+});
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es" className={poppins.className}>
+      <body className="font-sans antialiased">{children}</body>
+    </html>
+  );
+}
--- apps/mejores-restaurantes-frontend/src/config/sanity/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "7d9e3dzz",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "v1",
-  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
-  useCdn: true,
-});
